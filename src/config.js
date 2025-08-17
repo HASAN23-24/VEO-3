4bb5b949bd0080b0b9a7948269e612102c961a64
HASAN23-24 <bgoggle60@gmail.com>
2025-08-17 07:30:12 +0500

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+  PREFIX: process.env.PREFIX || ".",
+  BOT_NAME: process.env.BOT_NAME || "VEO3",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "03190217441"
+};
